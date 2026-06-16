80d5760f2d986bfb6f18bde58d9a5390b42a11e5
João Victor Ribeiro <joao.vsantos2@gmail.com>
2026-06-16 15:20:12 -0300

    nova pasta para exercicios aula 04

--- Exercicios aula 04/src/aula_04_repeticoes/ExercicioFor02.java
+++ Exercicios aula 04/src/aula_04_repeticoes/ExercicioFor02.java
@@ -5,29 +5,31 @@ import java.util.Scanner;
 public class ExercicioFor02 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner leia = new Scanner(System.in);
 
-		int [] numeros = new int [10];
-		
+		int[] numeros = new int[10];
+
 		int pares = 0, impares = 0;
-		
+
 		for (int contador = 0; contador < 10; contador++) {
 			System.out.print("Digite um número: ");
 			numeros[contador] = leia.nextInt();
 		}
-		
-		for (int contador = 0; contador < 10; contador++) { 
-			if (numeros[contador] % 2 == 0) { pares++; }
-			else { impares++;
-					}
-		}
-		 System.out.println("Pares: " + pares);
 
-	     System.out.println("Ímpares: " + impares);
-	     
-	     leia.close();
-		
+		for (int contador = 0; contador < 10; contador++) {
+			if (numeros[contador] % 2 == 0) {
+				pares++;
+			} else {
+				impares++;
+			}
+		}
+		System.out.println("Pares: " + pares);
+
+		System.out.println("Ímpares: " + impares);
+
+		leia.close();
+
 	}
 
 }
